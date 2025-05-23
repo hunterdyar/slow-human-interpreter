ac7569692678589c598a9e701ce877c7ec3a6edc
hunterdyar <bloop@hdyar.com>
2025-05-22 21:16:09 -0400

    actually use the prettier command names.

--- interpreter/IntermediateRep.py
+++ interpreter/IntermediateRep.py
@@ -1,26 +1,34 @@
 from enum import Enum
 
+
 class IntermediateRep:
     def __init__(self):
         self.routines = []
         self.routines.append([])
+
     def add_command(self, command):
         self.routines[-1].append(command)
-        return len(self.routines[-1])-1
+        return len(self.routines[-1]) - 1
+
     def update_argument(self, command_index, new_argument):
         self.routines[-1][command_index].argument = new_argument
+
     def get_top_index(self):
-        return len(self.routines[-1])-1
+        return len(self.routines[-1]) - 1
+
     # todo: make wrapper object for routine that gives routines names and threadability(?) status
     def push_routine(self):
         self.routines.append([])
+
     def get_top_routine_id(self) -> int:
-        return len(self.routines)-1
+        return len(self.routines) - 1
+
     def pop_routine(self):
         self.routines.pop()
 
     def __str__(self):
         return str(self.routines[0])
+
     def __repr__(self):
         return str(self.routines[0])
 
@@ -29,9 +37,10 @@ class IntermediateRep:
         num = 1
         for command in self.routines[0]:
             out["instructions"].append(command.get_pretty_object(num))
-            num +=1
+            num += 1
 
         return out
+
 
 class CommandType(Enum):
     PRINT = 0,
@@ -50,7 +59,8 @@ class CommandType(Enum):
     #JT = 13,
     #JZ = 14,
 
-commandNameLookup = {
+
+command_name_lookup = {
     CommandType.PUSH: "Push",
     CommandType.POP: "Pop",
     CommandType.ADD: "Add",
@@ -100,27 +110,27 @@ details = {
         "Place this answer on a card, and put on the stack.",
         "Discard A and B."
     ],
-    CommandType.PRINT:[
+    CommandType.PRINT: [
         "Take the top of the stack and place on A.",
         "Scream the value of the card on A as loudly as you can.",
         "Discard A"
     ],
-    CommandType.NOT:[
+    CommandType.NOT: [
         "Take the top of the stack and place on A.",
         "Negate the value. True becomes false and false becomes true.",
         "Place this negated value on the top of the stack.",
         "You will have replaced the top of the stack with it's opposite."
     ],
-    CommandType.CMP:[
+    CommandType.CMP: [
         "Take the top of the stack and place on A.",
         "Take the top of the stack and place on B.",
         "Compare A and B using the above operator. A is to the left and B is to the right.",
         "Take the result (which should be 'True' or 'False') and place on the top of the stack."
     ],
-    CommandType.JMP:[
+    CommandType.JMP: [
         "Turn the instructions to the above instruction number. (You will do that instruction next)."
     ],
-    CommandType.JF:[
+    CommandType.JF: [
         "Take the top of the stack and place it on A.",
         "If this value is True, discard the value and continue.",
         "If this value is False, None, or 0, turn the instructions to the above instruction number. Do that instruction next.",
@@ -141,6 +151,7 @@ argumentLookup = {
     "IsNot": "Is Not",
 }
 
+
 class Command:
     def __init__(self, command: CommandType, argument=None):
         self.command = command
@@ -156,7 +167,7 @@ class Command:
         return str(self.command)
 
     def get_pretty_object(self, num: int):
-        out = {"name": self.command.name,
+        out = {"name": command_name_lookup[self.command],
                "argument": self.get_pretty_argument(self.command, self.argument),
                "number": num,
                "details": details[self.command]
@@ -166,10 +177,10 @@ class Command:
     @staticmethod
     def get_pretty_argument(command, argument):
         if command == CommandType.CMP:
-             return argumentLookup[argument]
+            return argumentLookup[argument]
         if command == CommandType.PUSH:
-            if isinstance(argument,str):
-                return '"'+argument+'"'
+            if isinstance(argument, str):
+                return '"' + argument + '"'
             return str(argument)
         else:
             return argument
